4626fc707b0876ec852335eff182dac504a01560
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2017-11-02 07:11:07 +0900

    Attempting to build wheel

---
+++ build-wheel.sh
@@ -0,0 +1,7 @@
+#export CMAKE_FLAGS='-DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/opt/local/bin/python2.7 -DPYTHON_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 -DPYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib -DCMAKE_INSTALL_PREFIX=$HOME/local -DCMAKE_CXX_COMPILER=mpicxx-openmpi-clang39 -DCMAKE_VERBOSE_MAKEFILE=ON'
+export CMAKE_FLAGS='-DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/opt/local/bin/python2.7 -DPYTHON_INCLUDE_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 -DPYTHON_LIBRARY=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib -DCMAKE_CXX_COMPILER=mpicxx-openmpi-clang39 -DCMAKE_VERBOSE_MAKEFILE=ON'
+export CXXFLAGS='-std=c++11 -O1'
+
+#python2.7 ~/ClionProjects/ir_basis/setup.py build
+#python2.7 ~/ClionProjects/ir_basis/setup.py install --user
+python2.7 ~/ClionProjects/ir_basis/setup.py bdist_wheel
