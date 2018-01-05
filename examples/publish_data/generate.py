5b96e22c6736a2930f7b2748d744dead8de147ea
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-01-05 15:05:06 +0900

    Updated publish data

--- examples/publish_data/generate.py
+++ examples/publish_data/generate.py
@@ -6,13 +6,13 @@ import irlib
 # Compute basis functions very accurately
 
 max_dim = 1000
-cutoff = 1e-8
-a_tol = 1e-6
+cutoff = 1e-10
+a_tol = 1e-10
 verbose = True
 prec = 64
 
 ## Construct basis
-for Lambda in [100.0, 1000.0, 10000.0]:
+for Lambda in [100.0]:
     print("Computing basis functions... It may take some time")
     b = irlib.compute_basis(irlib.FERMIONIC, Lambda, max_dim, cutoff, "mp", a_tol, prec, verbose)
     print("Done!")
