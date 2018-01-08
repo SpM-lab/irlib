cb04df26b3c551e08e489e3ef8467af2c91ceb82
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-01-08 19:29:53 +0900

    Updated publish data

--- examples/publish_data/generate.py
+++ examples/publish_data/generate.py
@@ -12,7 +12,7 @@ verbose = True
 prec = 64
 
 ## Construct basis
-for Lambda in [100.0]:
+for Lambda in [100000.0]:
     print("Computing basis functions... It may take some time")
     b = irlib.compute_basis(irlib.FERMIONIC, Lambda, max_dim, cutoff, "mp", a_tol, prec, verbose)
     print("Done!")
