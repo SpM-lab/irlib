4e03c4fe9204474ca510b16c4db4c6439cdec140
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-02-21 15:34:56 -0500

    Updates samples

---
+++ samples/README.txt
@@ -0,0 +1,4 @@
+Run generate.py to compute basis functions.
+This may be very slow.
+
+To plot precomputed data, just use plot.py.
--- samples/plot.py
+++ samples/plot.py
@@ -17,19 +17,22 @@ idx = 0
 markers = ['o', 's', 'x', '+', 'v']
 ls = ['-', '--', ':']
 colors = ['r', 'b', 'g', 'k']
-for Lambda in [1000.0]:
+for Lambda in [10000.0]:
     #print("Loading basis functions...")
-    b = irlib.loadtxt("basis_b-mp-Lambda"+str(Lambda)+".txt")
+    b = irlib.loadtxt("basis_f-mp-Lambda"+str(Lambda)+".txt")
 
-    plt.figure(1)
     print("dim = ", b.dim())
 
+    plt.figure(1)
     for l in [0, 2, 4]:
         plt.plot(xvec, numpy.array([b.ulx(l,x) for x in xvec]), marker='', linestyle='-', color=colors[idx])
 
     plt.figure(2)
     for l in [0, 2, 4]:
         plt.plot(xvec, numpy.array([b.vly(l,x) for x in xvec]), marker='', linestyle='-', color=colors[idx], label='l='+str(l))
+
+    plt.figure(3)
+    plt.plot([b.sl(l)/b.sl(0) for l in range(b.dim())], marker='+', linestyle='-', color=colors[idx])
 
     idx += 1
 
@@ -43,9 +46,16 @@ plt.savefig('ulx.pdf')
 
 plt.figure(2)
 plt.xlabel('$y$')
-plt.ylabel('$v_l^\mathrm{B}(y)$')
-#plt.yscale("log")
+plt.ylabel('$v_l(y)$')
 plt.grid()
 plt.legend()
 plt.tight_layout()
 plt.savefig('vly.pdf')
+
+plt.figure(3)
+plt.xlabel('$l$')
+plt.ylabel('$s_l/s_0$')
+plt.legend()
+plt.yscale("log")
+plt.tight_layout()
+plt.savefig('sl.pdf')
