3f7072b5dc26c09c9a6a0e0824d76e261454ca38
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2018-03-29 11:18:14 +0900

    Some updates

--- samples/compare_np.py
+++ samples/compare_np.py
@@ -17,22 +17,25 @@ idx = 0
 markers = ['o', 's', 'x', '+', 'v']
 ls = ['-', '--', ':']
 colors = ['r', 'b', 'g', 'k']
-for Lambda in [10000.0]:
-    b10 = irlib.loadtxt("np10/basis_f-mp-Lambda"+str(Lambda)+".txt")
-    b8 = irlib.loadtxt("np8/basis_f-mp-Lambda"+str(Lambda)+".txt")
+for Lambda in [1000.0]:
+    b_np20 = irlib.loadtxt("np20/basis_f-mp-Lambda"+str(Lambda)+".txt")
+    b_np10 = irlib.loadtxt("np10/basis_f-mp-Lambda"+str(Lambda)+".txt")
 
-    print("dim = ", b10.dim(), b8.dim())
+    print("dim = ", b_np20.dim(), b_np10.dim())
 
     plt.figure(4)
     #nvec = numpy.arange(100)
-    nvec = numpy.array([0,10,10**2,400, 600, 800, 10**3, 2000, 4000, 6000, 10**4,10**5,10**6])
-    Tnl10 = b10.compute_Tnl(nvec)
-    Tnl8 = b8.compute_Tnl(nvec)
-    for l in [b10.dim()-1]:
-        plt.plot(nvec, numpy.abs(Tnl10[:,l]), marker='x', linestyle='-', color='r', label='l='+str(l))
-        plt.plot(nvec, numpy.abs(Tnl8[:,l]), marker='', linestyle='--', color='b', label='l='+str(l))
-        plt.plot(nvec, numpy.abs(Tnl8[:,l]-Tnl10[:,l]), marker='x', linestyle='-', color='r', label='abs error')
-        plt.plot(nvec, numpy.abs(Tnl8[:,l]-Tnl10[:,l])/numpy.abs(Tnl10[:,l]), marker='o', linestyle='-', color='r', label='relative error')
+    #nvec = numpy.array([0,10,10**2,400, 600, 800, 10**3, 2000, 4000, 6000, 10**4,10**5,10**6])
+    nvec = numpy.array([10,100])
+    Tnl_np20 = b_np20.compute_Tnl(nvec)
+    Tnl_np10 = b_np10.compute_Tnl(nvec)
+    for l in [b_np20.dim()-1]:
+        plt.plot(nvec, numpy.abs(Tnl_np20[:,l]), marker='x', linestyle='-', color='r', label='l='+str(l))
+        plt.plot(nvec, numpy.abs(Tnl_np10[:,l]), marker='', linestyle='--', color='b', label='l='+str(l))
+        plt.plot(nvec, numpy.abs(Tnl_np10[:,l]-Tnl_np20[:,l]), marker='x', linestyle='-', color='r', label='abs error')
+        plt.plot(nvec, numpy.abs(Tnl_np10[:,l]-Tnl_np20[:,l])/numpy.abs(Tnl_np20[:,l]), marker='o', linestyle='-', color='r', label='relative error')
+        print(Tnl_np10[:,-1])
+        print(Tnl_np20[:,-1])
         #plt.plot(nvec, Tnl[:,l].imag, marker='x', linestyle='-', color='r', label='l='+str(l))
         #plt.plot(nvec, Tnl20[:,l].imag, marker='', linestyle='--', color='b', label='l='+str(l))
 
